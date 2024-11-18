03983fe0255a52bcd34a3afb7b53067473979fd6
calvinji <calvinchenji@gmail.com>
2024-11-17 19:54:24 -0500

    Created an abstract class item that represents all items in the game.

---
+++ Item.java
@@ -0,0 +1,27 @@
+// Define an abstract class called Item (Not an interface because we need instance variables)
+public abstract class Item {
+    String name;
+    String description;
+    String type;
+
+
+    // Abstract method to interact with the item
+    public abstract void Interact();
+
+    // Regular method to pick up the item
+    public void Pickup() {
+        System.out.println("You picked up the item: " + name + ".");
+        // currentplayer.put_in_bag(name) #This section to be coded when all sections are put together and once we have a game with currentplayer as a global variable
+    }
+
+    // Regular method to inspect the item (has a body)
+    public void Inspect() {
+        System.out.println("Inspecting the item: " + name + ": " + description + ".");
+    }
+
+    // A method to return a string representation of the item
+    public String toStr(){
+        return name + ": " + description;
+    }
+}
+
