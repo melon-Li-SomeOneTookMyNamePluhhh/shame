582c737da60b5c9d6cfcb470cc576accad51dcc4
calvinji <calvinchenji@gmail.com>
2024-11-17 20:57:14 -0500

    Made changes to Key, a subclass of Item. Added a new instance variable Used to key (boolean), and made it private. Added a getter and setter for it. Also made changes to Interact to account for more cases and updated what gets printed to the terminal to make it more accurate.

--- Key.java
+++ Key.java
@@ -1,12 +1,26 @@
 public class Key extends Item{
     int damage;
     String element = "Regular";
+    private boolean Used = false; //has the key been used or not
+
+    public boolean getUsed(){
+        return Used;
+    }
+    public void setUsed(boolean used){
+        Used = used;
+    }
     @Override
     public void Interact() {
         // TODO: we can use this code once we have a game with currentplayer as a global variable and a location() method for all items.
-        if (currentplayer.location() == currentplayer.curr_room.exit.location() && currentroom.exit.locked == true){
-          currentplayer.curr_room.exit.locked = false;
+        if (currentplayer.location() == currentplayer.curr_room.exit.location() && currentroom.exit.isLocked() == true && this.getUsed() == false){
+          currentplayer.curr_room.exit.setLocked(false);
           System.out.println("You have unlocked the exit");
+        }
+        else if (currentplayer.location() == currentplayer.curr_room.exit.location() && currentroom.exit.isLocked() == true && this.getUsed() == true){
+            System.out.println("You are using the wrong key for the exit");
+        }
+        else if (currentplayer.location() == currentplayer.curr_room.exit.location() && currentroom.exit.isLocked() == false){
+            System.out.println("The exit you are at is already unlocked");
         }
         else {
           System.out.println("You are not standing at the exit; go to the correct exit to unlock the exit");
