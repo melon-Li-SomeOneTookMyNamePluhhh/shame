d821698f7cef218fd1d7f5e52bba0ff782c53306
calvinji <calvinchenji@gmail.com>
2024-11-17 21:24:56 -0500

    Added a constructor for Equipment. Equipment is a concrete subclass of Item and the constructor for Equipment calls the constructor of Item with super. There is a constructor that gives default damage of 1 and default element of Regular. We are using an overloaded constructor. There is another constructor where we have to set the damage and the element rather than just defaulting to default values for damage and element.

--- Equipment.java
+++ Equipment.java
@@ -1,6 +1,23 @@
 public class Equipment extends Item {
     int damage;
     String element;
+
+    // Constructor for Equipment that calls the Item constructor and has default damage and element
+    public Equipment(String name, String description) {
+        // Call the constructor of Item class with parameters
+        super(name, description, "Equipment"); // Pass values to the superclass constructor
+        this.damage = 1;
+        this.element = "Regular";
+    }
+
+    // Overloaded constructor for Equipment that calls the Item constructor and sets damage and element for the equipment
+    public Equipment(String name, String description, int damage, String element) {
+        // Call the constructor of Item class with parameters
+        super(name, description, "Equipment"); // Pass values to the superclass constructor
+        this.damage = damage;
+        this.element = element;
+    }
+
     @Override
     public void Interact() {
         System.out.println("This is a piece of equipment.");
