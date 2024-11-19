3bb81bbe5f70b376c8a0036601bd704c7f59d61f
calvinji <calvinchenji@gmail.com>
2024-11-18 20:32:57 -0500

    Debugged errors in Equipment

--- Item/Equipment.java
+++ Item/Equipment.java
@@ -7,7 +7,7 @@ public class Equipment extends Item {
     // Constructor for Item.Equipment that calls the Item.Item constructor and has default damage and element
     public Equipment(String name, String description, String floorDescription, int damage, String element) {
         // Call the constructor of Item.Item class with parameters
-        super(name, name, description, floorDescription, damage, "Item.Equipment"); // Pass values to the superclass constructor
+        super(name, description, floorDescription, "Equipment"); // Pass values to the superclass constructor
         this.damage = damage;
         this.element = element;
     }
@@ -15,5 +15,12 @@ public class Equipment extends Item {
     @Override
     public void Interact(Item location) {
         System.out.println("This is a piece of equipment.");
+    }
+
+
+    @Override
+    public Item pickup() {
+        System.out.println("You pickup" + this.getName());
+        return this;
     }
 }
