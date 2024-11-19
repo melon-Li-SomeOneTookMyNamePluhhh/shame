5d37f8d0ffa9df4eca0c4602104d67530301c398
Calvin Ji <81500827+Calvin-Ji@users.noreply.github.com>
2024-11-18 20:01:53 -0500

    Updated Item.java to fix the item constructor and to make pickup method abstract

--- Item/Item.java
+++ Item/Item.java
@@ -1,32 +1,16 @@
-package Item;
-
-// MAKE THIS INTO INTERFACE!!!!!!!!!!!!!!!!!!!!!!!!!!
-// Define an abstract class called Item.Item (Not an interface because we need instance variables)
 public abstract class Item {
     private String name;
     private String description;
     private String floorDescription;
     private String type;
-    private Boolean locked;
 
-    // This is a constructor for Item.Item; it cannot be directly instantiated but can be used by concrete subclasses.
-    public Item(String s, String name, String description, String floorDescription, int damage, String type) {
+    // This is a constructor for Item; it cannot be directly instantiated but can be used by concrete subclasses.
+    public Item(String name, String description, String floorDescription, String type) {
         this.name = name;
         this.description = description;
         this.floorDescription = floorDescription;
         // item on ground should have different discription compared with item picked up & in inventory
         this.type = type;
-        this.locked = false;
-    }
-    // overide when need to be locked
-    public Item(String name, String description, String floorDescription,String type, Boolean locked) {
-        this.name = name;
-        this.description = description;
-        this.floorDescription = floorDescription;
-        // item on ground should have different discription compared with item picked up & in inventory
-        this.type = type;
-        this.locked = locked;
-        //
     }
 
     // Abstract method to interact with the item
@@ -34,8 +18,7 @@ public abstract class Item {
 
     // Regular method to pick up the item
     // this method should to regularly overrided such that awkward moment of you picked up exit will not happen
-    public Item pickup() {
-        return null;
+    public abstract Item pickup() {
     }
 
     // Regular method to inspect the item (has a body)
