71f0f1d8df7690bf98826ca736367bb69770e3c9
calvinji <calvinchenji@gmail.com>
2024-11-18 20:30:36 -0500

    Debugged errors in Key

--- Item/Key.java
+++ Item/Key.java
@@ -1,27 +1,23 @@
 package Item;
 
 public class Key extends Item{
-    int damage;
+    int damage = 1;
     String element = "Regular";
 
     // Constructor for Item.Key that calls the Item.Item constructor
     public Key(String name, String description, String floorDescription, int damage, String element) {
         // Call the constructor of Item.Item class with parameters
-        super(name, description, floorDescription,"Key", damage, element); // Pass values to the superclass constructor
+        super(name, description, floorDescription,"Key"); // Pass values to the superclass constructor
+        this.damage = damage;
+        this.element = element;
     }
 
     @Override
     public void Interact(Item player_location) {
         // TODO: We should have a game with the current player as a global variable. A current player will have a location.
-        if (player_location.gettype() == "Exit" && player_location.getLocked() == true && this.getused() == false){
-          location.setLocked(false);
+        if (player_location.getType() == "Exit" && player_location.getLocked() == true){
+          player_location.setLocked(false);
           System.out.println("You have unlocked the exit");
-        }
-        else if (player_location.gettype() == "Exit" && player_location.getLocked() == true && this.getused() == true){
-                System.out.println("You are using the wrong key for this exit.");
-        }
-        else if (player_location.gettype() == "Exit" && player_location.getLocked() == false){
-                System.out.println("The exit you are at is already unlocked!");
         }
         else {
           System.out.println("You are not standing at an exit!");
