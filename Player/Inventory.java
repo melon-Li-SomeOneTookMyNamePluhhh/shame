2c7ed8e9c07ca8320e431ac2e1181efa241d380d
Liu-MELODY1 <161888068+Liu-MELODY1@users.noreply.github.com>
2024-11-24 09:03:30 +0800

    change remove item and find item

--- Player/Inventory.java
+++ Player/Inventory.java
@@ -2,10 +2,11 @@ package Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import Item.Item;
 
 public class Inventory {
     private int bagSize;
-    private List<String> itemsInside;
+    private List<Item> itemsInside;
 
     public Inventory(int bagSize) {
         this.bagSize = bagSize;
@@ -13,24 +14,44 @@ public class Inventory {
     }
 
     public int getBagSize() {
+
         return bagSize;
     }
 
     public int getNumItemsInside() {
+
         return itemsInside.size();
     }
 
-    public List<String> getItemsInside() {
+    public List<Item> getItemsInside() {
+
         return itemsInside;
     }
 
     // Add item
-    public void addItem(String item) {
+    public void addItem(Item item) {
+
         itemsInside.add(item);
     }
 
     // Remove item
-    public void removeItem(String item) {
-        itemsInside.remove(item);
+    public Item removeItem(String itemName) {
+        for (int i = 0; i < itemsInside.size(); i++) {
+            Item item = itemsInside.get(i);
+            if (item.getName().equals(itemName)) {
+                itemsInside.remove(i);
+                return item;
+            }
+        }
+        return null; // 如果未找到对应的Item，返回null
+    }
+
+    public Item findItem(String itemName){
+        for (Item item : itemsInside){
+            if (item.getName().equals(itemName)){
+                return item;
+            }
+        }
+        return null; // 如果未找到对应的Item，返回null
     }
 }
