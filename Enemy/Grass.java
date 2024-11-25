31aea28e9dd5d7cee1601b7b6280106add9d95bb
Liu-MELODY1 <161888068+Liu-MELODY1@users.noreply.github.com>
2024-11-26 07:04:49 +0800

    improve enemy, make fire grass water could return damage(which could be used by battle part), change a lot for enemy class and its child class

--- Enemy/Grass.java
+++ Enemy/Grass.java
@@ -1,20 +1,22 @@
 package Enemy;
 
 public class Grass extends Enemy {
-    private boolean usedSpecialAbility = false;
 
     public Grass(String name, String description) {
-        super(name, description, new AttackList());
+        super(name, description, new AttackList(), "Grass"); // Set type to "Grass"
     }
 
     @Override
     public void useSpecialAbility() {
-        if (!usedSpecialAbility && health < 5) {
-            health += 3;
-            System.out.println("GrassEnemy heals itself! Gains 3 health. Current health: " + health);
-            usedSpecialAbility = true;
-        } else {
-            System.out.println("Special ability cannot be used.");
-        }
+        System.out.println(name + " used its special ability, healing 3 health points.");
+        health += 3;
+        usedSpecialAbility = true; // Mark that special ability has been used
     }
+
+    @Override
+    protected boolean shouldUseSpecialAbility() {
+        return health < 5 && !usedSpecialAbility;
+    }
+
+    // No need to override performAction() as special ability doesn't affect attack logic
 }
