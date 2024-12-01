0be240c03f63b8db4a4ab1f99d2ab5d83848baf7
Alice <yujun.lu@mail.utoronto.ca>
2024-12-01 13:16:52 -0500

    CA User_case

--- src/main/java/User_case/PlayerUsercase/outputBoundaryInterface.java
+++ src/main/java/User_case/PlayerUsercase/outputBoundaryInterface.java
@@ -1,4 +1,23 @@
 package User_case.PlayerUsercase;
 
+import java.util.List;
+import Entity.Item;
+
+/**
+ * The outputBoundaryInterface defines methods for displaying messages and
+ * player-related information, such as bag contents, in the game.
+ */
 public interface outputBoundaryInterface {
+
+    /**
+     * Displays a general message to the player.
+     * @param message The message to be displayed.
+     */
+    void displayMessage(String message);
+
+    /**
+     * Displays the contents of the player's bag, listing each item by name and description.
+     * @param items The list of items in the player's bag.
+     */
+    void displayBagContents(List<Item> items);
 }
