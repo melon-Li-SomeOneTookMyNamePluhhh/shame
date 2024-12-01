4ac403a6842eca88c3df7952fb759075dbcf3aa3
Calvin Ji <81500827+Calvin-Ji@users.noreply.github.com>
2024-11-30 20:22:58 -0500

    Update NPC.java
    Added overloaded constructor that gets the riddle directly from the api call

--- Item/NPC.java
+++ Item/NPC.java
@@ -1,18 +1,34 @@
 package Item;
 
+import Item_with_subclasses_and_API.RiddleFetcher;
 import GUI.GUIUtility;
-
 import java.util.Scanner;
 
 public class NPC extends Item {
     String Riddle;
     String Speech;
+    String Answer;
 
     // Constructor for Item.NPC that calls the Item.Item constructor
-    public NPC(String name, String description, String floorDescription, String Riddle, String Speech) {
+    public NPC(String name, String description, String floorDescription, String Riddle, String Answer, String Speech) {
         // Call the constructor of Item.Item class with parameters
         super(name, description, floorDescription,"NPC"); // Pass values to the superclass constructor
         this.Riddle = Riddle;
+        this.Answer = Answer;
+        this.Speech = Speech;
+    }
+    // Overloaded constructor that gets the riddle directly from the api call
+    public NPC(String name, String description, String floorDescription, String Speech) {
+        // Call the constructor of Item.Item class with parameters
+        super(name, description, floorDescription,"NPC"); // Pass values to the superclass constructor
+
+        // Get the riddle and answer from RiddleFetcher
+        String[] riddleData = RiddleFetcher.fetchRiddleAndAnswer();
+
+        // Assign the fetched riddle and answer to the fields
+        this.Riddle = riddleData[0];  // riddle
+        this.Answer = riddleData[1];  // answer
+
         this.Speech = Speech;
     }
 
@@ -61,3 +77,5 @@ public class NPC extends Item {
         return null;
     }
 }
+
+
