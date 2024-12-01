701838df2b27cb8bcd43abd7eaa30b702a94549c
calvinji <calvinchenji@gmail.com>
2024-11-30 21:04:41 -0500

    Added a User Story java file for all 5 group members. The files can be found in the User_Stories folder and have some basic starter code that makes it easier for everyone to get started and start coding their user story. This includes most of the imports from enemy, DungenKeeper, Item, and GUI that you will need as well as a main method with a few lines of starter code that sets up the validActions.

---
+++ User_Stories/Cynthia_User_Story.java
@@ -0,0 +1,31 @@
+package User_Stories;
+
+import DungenKeeper.DungenBuilder;
+import DungenKeeper.Room;
+import Enemy.Enemy;
+import Enemy.Fire;
+import Enemy.Water;
+import Enemy.Grass;
+import Item.Equipment;
+import Item.Exit;
+import Item.Key;
+import GUI.GUIUtility;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class Cynthia_User_Story {
+
+    public static void main(String[] args) {
+        // Define valid actions
+        Set<String> validActions = new HashSet<>();
+        validActions.add("walk to");
+        validActions.add("pick up");
+        validActions.add("speech");
+        validActions.add("exit");
+
+        //Finish user story
+
+    }
+}
