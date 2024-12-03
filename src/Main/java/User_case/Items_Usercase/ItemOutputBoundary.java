d65666b81f2e5dd297fa23f7a368321f9f9fcc35
Calvin Ji <81500827+Calvin-Ji@users.noreply.github.com>
2024-12-02 19:47:33 -0500

    Update ItemOutputBoundary.java
    Update to fix tests

--- src/Main/java/User_case/Items_Usercase/ItemOutputBoundary.java
+++ src/Main/java/User_case/Items_Usercase/ItemOutputBoundary.java
@@ -1,14 +1,19 @@
 package User_case.Items_Usercase;
 
 public class ItemOutputBoundary implements ItemOutputBoundaryInterface {
-
+    public String message = " ";
     /**
      * Displays a general message to the user.
      * @param message The message to be displayed.
      */
     public void displayOutput(String message){
-        Frameworks_and_drivers.guiUtility guiUtility = new Frameworks_and_drivers.guiUtility();
-        guiUtility.displayOutput(message);
+
+        System.out.println(message);
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return this.message;
     }
 
 }
